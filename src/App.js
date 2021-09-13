1d9acb3319cc8441ecb4f84cc8074c2ca14907b2
Natalia Aronson <nataronso@gmail.com>
2021-09-13 15:10:39 +0200

    adding Gallery

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from'react';
 import './App.css';
-import VerbsCard from './components/VerbsCard/VerbsCard';
+import Gallery from './components/VerbsCard';
 import VocList from './components/vocList/vocList';
 
 function App() {
@@ -8,7 +8,7 @@ function App() {
     <div className="App">
     
      <h1> Learn English</h1>
-       <VerbsCard/>  
+       <Gallery/>  
       <VocList/>
 
      
